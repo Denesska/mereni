fd9439d7537e55be2091f0263ac18c46aa6b2d09
Denis Gandzii <denesska@gmail.com>
2017-06-22 19:24:40 +0300

    Modify email column name length2

--- public/test.php
+++ public/test.php
@@ -10,7 +10,7 @@ if (isset($_POST['submit'])){
     $activity = escaped_str($_POST['activity']);
     $now = date("Y-m-d H:i:s");
   //  $query = "INSERT INTO loc_activitate (locatie, added) VALUES ('$activity', '$now' )";
-    $query = "ALTER TABLE cozagro_db.login MODIFY email VARCHAR(50) NOT NULL;', '$now' )";
+    $query = "ALTER TABLE cozagro_db.login MODIFY email VARCHAR(50) NOT NULL";
     $result = Database::getInstance()->getConnection()->query($query);
     if (!$result){
         $_SESSION['message'] = "<br>Eroare la adaugare activitate: ".Database::getInstance()->getConnection()->error;
